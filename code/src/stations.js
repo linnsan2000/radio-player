4c4eb4bf43e5fc3509d6e0bae7d1b598af2fb54b
linnsan2000 <31280120+linnsan2000@users.noreply.github.com>
2017-12-05 15:35:04 +0100

    Hej

--- code/src/stations.js
+++ code/src/stations.js
@@ -8,12 +8,11 @@ class Stations extends React.Component {
           <img src={this.props.image} alt="Radiokanalens logotyp" />
           <h1>{this.props.name}</h1>
           <p>{this.props.tagline}</p>
+          <h3>Direktsändning från {this.props.name}:</h3>
           <audio controls>
             <source src={this.props.liveaudio} type="audio/mp3" />
               Your browser does not support the audio element.
           </audio>
-          <a href="{this.props.siteurl}">{this.props.name}:s sajt</a>
-          <a href="{this.props.schedule}">{this.props.name}:s tablå</a>
         </div>
       </div>
     )
